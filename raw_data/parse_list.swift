a60638b3d4d6e1434a07a080a060ce21b5724045
Dustin Pfannenstiel <dustin@nth-estate.com>
2018-10-13 18:04:20 -0500

    updating parse lists

--- raw_data/parse_list.swift
+++ raw_data/parse_list.swift
@@ -2,27 +2,19 @@
 
 import Foundation
 
-let sample = """
-<-- NEW CARD -->
-Title: Assault Guru
-Collector's Number: 1 C 4
-Faction: Earther
-Card Type: CHARACTER ¥ CISyn ¥ Tech ¥ Unit
-Cost: 2
-Destiny: 4
-Support Cost: 1 Earther
-Power: 1
-Tactics: 1
-Defense: 3
-Game Text: As long as this character is at a site where you have a higher total tactics than your opponent, you cause attrition +2 here.
-Lore: Sana Camrik is an assault guru for CISyn. She received her promotion for her valor during the Battle of Phobos.
-ILLUSTRATOR: PAMELINA H
-"""
+// parse arguments
 
-if let data = sample.data(using: .utf8) {
-	let inputStream = InputStream(data:data)
-	inputStream.open()
-	print(inputStream.hasBytesAvailable)
-	inputStream.close()
+var output_file = "wars.json"
+
+if CommandLine.argc < 2 {
+	print("No parse file provided.")
+	exit(1)
 }
 
+// I'm not getting too damn fancy with this, read the file from the same path as the script.
+// also run the script from the path its in.  You should only have to do this once.
+// then save the crap outta it.  Frankly, why am I even passing in the filename...
+let inputInputFile = CommandLine.argv.last!
+do {
+	let warsInput = try String()
+}
